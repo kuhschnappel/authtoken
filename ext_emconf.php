bb44f2502b9823f28b1edcd9d3f8f3881516b88c
Mike Zimmer <mike.zimmer@thieleundklose.de>
2023-10-28 15:47:39 +0200

    increase typo3 dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '2.1.2',
 	'constraints' => [
 		'depends' => [
-            'typo3' => '12.4.0-12.4.99',
+            'typo3' => '11.5.0-12.4.99',
             'php' => '7.4.1-8.1.99',
         ],
         'conflicts' => [],
